01fb924f8c636568f24b12ce2bbc03787892447d
qmkimball <quinnkimball192@gmail.com>
2021-11-29 12:05:37 -0700

    Added Movie table and Movie Schema

--- app.py
+++ app.py
@@ -12,6 +12,26 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    genre = db.Column(db.String, nullable=False)
+    mpaa_rating = db.Column(db.String)
+    poster_image = db.Column(db.String, unique=True)
+
+    def __init__(self, title, genre, mpaa_rating, poster_image):
+        self.title = title
+        self.genre = genre
+        self.mpaa_rating = mpaa_rating
+        self.poster_imagae = poster_image
+
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'genre', 'mpaa_rating', 'poster_image')
+
+movie_schema = MovieSchema()
+multi_movie_schema = MovieSchema(many=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
